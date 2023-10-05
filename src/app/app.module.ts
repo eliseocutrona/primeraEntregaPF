9dfbb4d33dc68e2d0427eb5bca6b43965f0a9748
eliseocutrona <eliseocutrona@gmail.com>
2023-10-05 00:25:21 -0300

    ABM

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,4 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
